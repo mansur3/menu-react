68af7c6d8c3a55068901a90c7937c6966cf6f44f
Mansur Dewan <mansurdewan26@gmail.com>
2021-10-19 21:49:23 +0530

    today menu with react

---
+++ src/components/Food.js
@@ -0,0 +1,41 @@
+import "./food.css";
+
+
+
+
+function Food({image, name, description, calory, date, celcius, price, discountrate, icon1, icon2}) {
+    return (
+        <div className = "container">
+            <div className = "image-container">
+                <img className = "image" src = {image} alt = {name} />
+            </div>
+            <div className = "product-details">
+                <div className = "product-one">
+                    <h1 className = "product-name">{name}</h1>
+                    <div className = "product-icon">
+                        <img className = "product-icon-one" src = {icon1} alt = "icon1" />
+                        <img className = "product-icon-two" src = {icon2} alt = "icon2" />
+                    </div>
+                </div><br />
+                <div className = "product-two">
+                    <p className = "product-desc">
+                        {description}
+                    </p>
+                </div>
+                <div className = "product-three">
+                    <p className = "product-calory">{calory} Cal </p>
+                    <p className = "product-date">P/F/C: {date}</p>
+                    <p className = "product-celcius">{celcius} °c</p>
+                </div>
+                <div className = "product-four">
+                    <h2 className = "product-price">${price} </h2>
+                    <h2 className = "product-discount">${discountrate}</h2>
+                    <button className = "product-order">ORDER</button>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Food;
